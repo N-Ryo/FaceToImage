10f4342dcdc199825e3b7c10fe63d466fd7a8656
N-Ryo <ryo.joy.joy@gmail.com>
2020-04-13 06:31:20 +0900

    create UserBook table

--- back/db/migrate/20200412212930_create_user_books.rb
+++ back/db/migrate/20200412212930_create_user_books.rb
@@ -1,10 +1,10 @@
 class CreateUserBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :user_books do |t|
-      t.integer :user_id
-      t.integer :book_id
-      t.integer :page_id
-      t.boolean :stucked
+      t.integer :user_id, foreign_key: true, null: false
+      t.integer :book_id, foreign_key: true, null: false
+      t.integer :page_id, foreign_key: true, null: false
+      t.boolean :stucked, default: false
 
       t.timestamps
     end
